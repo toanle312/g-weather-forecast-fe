6cba551601d570ad41328e2e12ffb7ced58087cb
toanle312 <toanle3122002@gmail.com>
2024-03-31 09:49:43 +0700

    Fix error when loading page

--- src/pages/WeatherPage.jsx
+++ src/pages/WeatherPage.jsx
@@ -24,7 +24,7 @@ const WeatherPage = () => {
 
   return (
     <div className='flex flex-col gap-2'>
-      {isLoading ? (
+      {isLoading || !currentLocation ? (
         <Loading />
       ) : weatherData && weatherData.error ? (
         <WeatherNotFound />
